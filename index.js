6618e06b10440dbeb1ae28fa363252c0c3b06a36
Michael Hranek <mhranek@comcast.net>
2022-01-05 17:28:37 -0500

    Populated list of questions

--- index.js
+++ index.js
@@ -2,7 +2,53 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description?',
+    },
+    {
+        type: 'input',
+        name: 'installInstruct',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'useageInfo',
+        message: 'What is the useage information?',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'What are the contributions?',
+    },
+    {
+        type: 'input',
+        name: 'testInstruct',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license do you want?',
+    },
+    {
+        type: 'input',
+        name: 'gitHubUser',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
